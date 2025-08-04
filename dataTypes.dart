b1af91640008089fdcc3d4ef31b5b49a01e0d661
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-04 08:11:18 +0700

    data types

---
+++ dataTypes.dart
@@ -0,0 +1,19 @@
+void main() {
+
+  /**
+   * Dart has special support for these data types
+   * 
+   * 1. Numbers ( int, double )
+   * 2. Strings
+   * 3. Booleans
+   * 4. Lists
+   * 5. Maps
+   * 6. Runes ( for expressing Unicode characters )
+   * 7. Symbols
+   * 
+   * NOTE : All data types in Dart are objects
+   * Therefore, values are by default null
+   */
+
+
+}
