e23ec200c2c39bf00853bc725a3dda134b46afcb
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-04 08:37:08 +0700

    constant keywords

--- variables.dart
+++ variables.dart
@@ -5,4 +5,32 @@ void main() {
   var age = 21;
 
   print("My name is $fullName and I am $age years old");
+
+
+  // Constants: final and const keywords
+  /**
+   * Difference between final and const
+   * 
+   * 1. final variable can only be set once and it is initialized when accessed
+   * 2. const variable is initialized during compilation
+   * 
+   */
+
+  // final keyword
+  final String capitalCity = "Jakarta";
+  final String country = "Indonesia";
+
+  // const keyword
+  const double pi = 3.14;
+  const double gravity = 9.8;
+}
+
+
+class Person{
+
+  final String firstName = "John";
+  
+  // instance variable can be final but cannot be cons
+  static const String lastName = "Doe";
+
 }
